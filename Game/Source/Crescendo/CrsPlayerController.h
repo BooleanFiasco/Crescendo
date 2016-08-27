04ae9a7c548648d7d520d0fd06572c58cf7bbeeb
Norm Nazaroff <norm.nazaroff@gmail.com>
2016-08-27 19:39:17 -0400

    Some new assets, mostly anim montages.

---
+++ Game/Source/Crescendo/CrsPlayerController.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/PlayerController.h"
+#include "CrsPlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CRESCENDO_API ACrsPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
